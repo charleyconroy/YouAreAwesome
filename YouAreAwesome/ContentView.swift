f0298d793cca37fc7b31b664d8e3749556063289
Charles Conroy <conroycd@bc.edu>
2023-01-23 17:36:51 -0500

    finished 1.6

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,19 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var messageString = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            //Hi Brenden
+            
+            //SWIFT LOGO CODE
+            //            Image(systemName: "swift")
+            //                .resizable()
+            //                .scaledToFit()
+            //            //.imageScale(.large)
+            //                .foregroundColor(.orange)
+            //                .padding()
+            Text(messageString)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundColor(.red)
+                .padding()
+            //FABULOUS TEXT CODE
+            //            Text("Fabulous? That's You!")
+            //                .font(.largeTitle)
+            //                .fontWeight(.heavy)
+            //                .foregroundColor(.red)
+            HStack {
+                Button("Awesome!") {
+                    //This is the action performed when the Awesome button is pressed
+                    messageString = "You Are Awesome!"
+                }
+                .buttonStyle(.borderedProminent)
+                Button("Great") {
+                    //This is the action performed when the Great button is pressed
+                    messageString = "You Are Great!"
+                }
+                .buttonStyle(.borderedProminent)
+                
+            }
         }
-        .padding()
     }
 }
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
-}
